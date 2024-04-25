06afa7c24cc632a78a76b78dce6b9b5bfeeef2aa
Sasikant Biswal <biswalsashikant@gmail.com>
2024-04-26 02:49:24 +0530

    Added some validation error tests

--- backend/src/user/UserRouter.js
+++ backend/src/user/UserRouter.js
@@ -2,9 +2,41 @@ const express = require("express");
 const router = express.Router();
 const UserService = require("./UserService");
 
-router.post("/api/1.0/users", async (req, res) => {
+const validateUsername = (req, res, next) => {
+  const user = req.body;
+  if (user.username === null) {
+    req.validationErrors = {
+      username: "Username cannot be null",
+    };
+  }
+
+  next();
+};
+
+const validateEmail = (req, res, next) => {
+  const user = req.body;
+  if (user.email === null) {
+    req.validationErrors = {
+      ...req.validationErrors,
+      email: "Email cannot be null",
+    };
+  }
+
+  next();
+};
+
+router.post(
+  "/api/1.0/users",
+  validateUsername,
+  validateEmail,
+  async (req, res) => {
+    if (req.validationErrors) {
+      const response = { validationErrors: { ...req.validationErrors } };
+      return res.status(400).send(response);
+    }
     await UserService.save(req.body);
-  return res.status(200).send({ message: "User created" });
-});
+    return res.status(200).send({ message: "User created" });
+  },
+);
 
 module.exports = router;
